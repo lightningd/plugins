012378e020c013e3a9fc6b9f2c6abb866291a5ac
darosior <darosior@protonmail.com>
2019-12-16 22:03:29 +0100

    monitor: add basic starting test

---
+++ monitor/test_monitor.py
@@ -0,0 +1,16 @@
+import os
+from pyln.testing.fixtures import *  # noqa: F401,F403
+
+plugin_path = os.path.join(os.path.dirname(__file__), "monitor.py")
+
+
+def test_helpme_starts(node_factory):
+    l1 = node_factory.get_node()
+    # Test dynamically
+    l1.rpc.plugin_start(plugin_path)
+    l1.rpc.plugin_stop(plugin_path)
+    l1.rpc.plugin_start(plugin_path)
+    l1.stop()
+    # Then statically
+    l1.daemon.opts["plugin"] = plugin_path
+    l1.start()
