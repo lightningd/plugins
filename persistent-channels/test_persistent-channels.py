c6e3b9f0118a884f3e530dcb6be7c8c935239f8c
Christian Decker <decker.christian@gmail.com>
2019-12-27 15:58:14 +0100

    pytest: Rename start tests to reflect the plugin under test

--- persistent-channels/test_persistent-channels.py
+++ persistent-channels/test_persistent-channels.py
@@ -4,7 +4,7 @@ from pyln.testing.fixtures import *  # noqa: F401,F403
 plugin_path = os.path.join(os.path.dirname(__file__), "persistent-channels.py")
 
 
-def test_helpme_starts(node_factory):
+def test_persistent_channels_starts(node_factory):
     l1 = node_factory.get_node()
     # Test dynamically
     l1.rpc.plugin_start(plugin_path)
