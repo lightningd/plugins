2bc8680c62fe9857812aaa886702fee3bf5b418d
darosior <darosior@protonmail.com>
2020-02-06 15:29:43 +0100

    Initial version of an esplora backend plugin

---
+++ sauron/art.py
@@ -0,0 +1,26 @@
+# Art source: https://textart.io/art/kF4RP1GLcmBNgF2zVV3_JQeF/lord-of-the-rings-eye-of-the-sauron
+sauron_eye = """
+
+                     Three::rings
+                for:::the::Elven-Kings
+             under:the:sky,:Seven:for:the
+           Dwarf-Lords::in::their::halls:of
+          stone,:Nine             for:Mortal
+         :::Men:::     ________     doomed::to
+       die.:One   _,-'...:... `-.    for:::the
+       ::Dark::  ,- .:::::::::::. `.   Lord::on
+      his:dark ,'  .:::::zzz:::::.  `.  :throne:
+      In:::the/    ::::dMMMMMb::::    \ Land::of
+      :Mordor:\    ::::dMMmgJP::::    / :where::
+      ::the::: '.  '::::YMMMP::::'  ,'  Shadows:
+       lie.::One  `. ``:::::::::'' ,'    Ring::to
+       ::rule::    `-._```:'''_,-'     ::them::
+       all,::One      `-----'        ring::to
+         ::find:::                  them,:One
+          Ring:::::to            bring::them
+            all::and::in:the:darkness:bind
+              them:In:the:Land:of:Mordor
+                 where:::the::Shadows
+                      :::lie.:::
+
+"""
