5a621e1edd50eb0cb3e8fa66526e7bb956ec61f8
Michal Rostecki <mrostecki@mailfence.com>
2020-04-04 17:35:01 +0200

    backup: Add Dropbox backend
    Dropbox backend for the backup plugin. It gives a possibility to save
    synchronous database backups to Dropbox. The backup, before sending to
    Dropbox, is optionally encrypted with PGP.
    
    Signed-off-by: Michal Rostecki <mrostecki@mailfence.com>

--- backup/backup.py
+++ backup/backup.py
@@ -10,7 +10,12 @@ import os
 import struct
 import sys
 import sqlite3
+import dropbox
+import tempfile
+import gnupg
 
+
+TMP_BACKUP_NAME = "backup.dbak"
 
 plugin = Plugin()
 
@@ -33,7 +38,8 @@ Change = namedtuple('Change', ['version', 'snapshot', 'transaction'])
 
 
 class Backend(object):
-    def __init__(self, destination: str):
+    def __init__(self, destination: str, options: Mapping[str, str],
+                 create: bool):
         """Read the metadata from the destination and prepare any necesary resources.
 
         After this call the following members must be initialized:
@@ -119,9 +125,12 @@ class Backend(object):
             if c.transaction is not None:
                 self._restore_transaction(c.transaction)
 
+    def validate_options(self, options: Mapping[str, str]):
+        raise NotImplementedError
 
 class FileBackend(Backend):
-    def __init__(self, destination: str, create: bool):
+    def __init__(self, destination: str, options: Mapping[str, str],
+                 create: bool):
         self.version = None
         self.prev_version = None
         self.destination = destination
@@ -139,6 +148,18 @@ class FileBackend(Backend):
             return False
         return self.read_metadata()
 
+    def validate_options(self, options: Mapping[str, str]):
+        pass
+
+    def path_metadata(self):
+        return self.url.path
+
+    def path_current(self):
+        return self.url.path
+
+    def path_stream(self):
+        return self.url.path
+
     def write_metadata(self):
         blob = struct.pack("!IIQIQQ", 0x01, self.version, self.offsets[0],
                            self.prev_version, self.offsets[1],
@@ -146,15 +167,15 @@ class FileBackend(Backend):
 
         # Pad the header
         blob += b'\x00' * (512 - len(blob))
-        mode = "rb+" if os.path.exists(self.url.path) else "wb+"
+        mode = "rb+" if os.path.exists(self.path_metadata()) else "wb+"
 
-        with open(self.url.path, mode) as f:
+        with open(self.path_metadata(), mode) as f:
             f.seek(0)
             f.write(blob)
             f.flush()
 
     def read_metadata(self):
-        with open(self.url.path, 'rb') as f:
+        with open(self.path_metadata(), 'rb') as f:
             blob = f.read(512)
             if len(blob) != 512:
                 logging.warn("Corrupt FileBackend header, expected 512 bytes, got {} bytes".format(len(blob)))
@@ -178,7 +199,7 @@ class FileBackend(Backend):
 
         length = struct.pack("!I", len(payload))
         version = struct.pack("!I", entry.version)
-        with open(self.url.path, 'ab') as f:
+        with open(self.path_current(), 'ab') as f:
             f.seek(self.offsets[0])
             f.write(length)
             f.write(version)
@@ -207,7 +228,7 @@ class FileBackend(Backend):
     def stream_changes(self) -> Iterator[Change]:
         self.read_metadata()
         stop = self.offsets[0]
-        with open(self.url.path, 'rb') as f:
+        with open(self.path_stream(), 'rb') as f:
             # Skip the header
             f.seek(512)
             while f.tell() < stop:
@@ -224,23 +245,167 @@ class FileBackend(Backend):
 
 
 
+class PartialFileBackend(FileBackend):
+    def initialize(self) -> bool:
+        if len(os.listdir(self.working_dir())) == 0:
+            self.version = 0
+            self.prev_version = 0
+            return False
+        return self.read_metadata()
+
+    def working_dir(self):
+        return os.path.dirname(self.url.path)
+
+    def path_metadata(self):
+        return "{}.0".format(self.url.path)
+
+    def path_current(self):
+        return "{}.{}".format(self.url.path, self.version)
+
+    def path_stream(self):
+        return self.url.path
+
+    def stream_changes(self) -> Iterator[Change]:
+        if os.path.exists(self.path_stream()):
+            os.remove(self.path_stream())
+        files = sorted(os.listdir(self.working_dir()),
+                       key=lambda x: int(x.split(".")[-1]))
+        with open(self.path_stream(), 'wb') as f:
+            for partial in files:
+                with open(os.path.join(self.working_dir(), partial), 'rb') as fp:
+                    f.write(fp.read())
+        return super().stream_changes()
+
+
+
+class DropboxBackend(PartialFileBackend):
+    def __init__(self, destination: str, options: Mapping[str, str],
+                 create: bool):
+        # Create a temporary directory in which the last downloaded backup will
+        # be stored. Then use the FileBackend to operate on that file.
+        self.tmpdir = tempfile.TemporaryDirectory(prefix="clightning-backup")
+        super().__init__(destination, options, create)
+        self.dropbox_url = urlparse(destination)
+        self.dbx = dropbox.Dropbox(options["dropbox-token"])
+        self.gpg = gnupg.GPG()
+        self.pgp_key = None
+        if options["pgp-key"] != "null":
+            self.pgp_key = options["pgp-key"]
+
+    def dropbox_dir(self):
+        dd = os.path.dirname(self.url.path)
+        # Dropbox API expects an empty string when accessing the main dir...
+        if dd == "/":
+            return ""
+        return dd
+
+    def dropbox_path(self, filename):
+        # ..but it expects "/" when getting the file from the main dir...
+        return os.path.join(os.path.dirname(self.url.path), filename)
+
+    def working_dir(self):
+        return self.tmpdir.name
+
+    def base_filename(self):
+        return os.path.basename(self.url.path)
+
+    def path_metadata(self):
+        return os.path.join(self.working_dir(),
+                            "{}.0".format(self.base_filename()))
+
+    def dropbox_path_metadata(self):
+        return "{}.0".format(self.dropbox_url.path)
+
+    def path_current(self):
+        return os.path.join(self.working_dir(),
+                            "{}.{}".format(self.base_filename(), self.version))
+
+    def dropbox_path_current(self):
+        return "{}.{}".format(self.dropbox_url.path, self.version)
+
+    def path_stream(self):
+        return os.path.join(self.working_dir(), self.base_filename())
+
+    def decrypt_file(self, filename):
+        with open(filename, 'rb') as f:
+            # I would love to use gpg.decrypt_file as it seems to handle
+            # buffering properly, but it's crashing on Python 3.8...
+            content = self.gpg.decrypt(f.read()).data
+        with open(filename, 'wb') as f:
+            f.write(content)
+
+    def encrypt_file(self, filename):
+        with open(filename, 'rb') as f:
+            content = f.read()
+        with open(filename, 'wb') as f:
+            f.write(self.gpg.encrypt(content, self.pgp_key).data)
+
+    def initialize(self) -> bool:
+        files = [f.name for f in
+                 self.dbx.files_list_folder(self.dropbox_dir()).entries
+                 if self.base_filename() in f.name]
+        if len(files) == 0:
+            self.version = 0
+            self.prev_version = 0
+            return False
+        for f in files:
+            self.dbx.files_download_to_file(os.path.join(self.working_dir(), f),
+                                            self.dropbox_path(f))
+        if self.pgp_key:
+            for f in files:
+                self.decrypt_file(os.path.join(self.working_dir(), f))
+        return self.read_metadata()
+
+    def validate_options(self, options: Mapping[str, str]):
+        if options['dropbox-token'] == 'null':
+            abort("You must specify access token for Dropbox")
+
+    def write_metadata(self):
+        super().write_metadata()
+        if self.pgp_key:
+            self.encrypt_file(self.path_metadata())
+        with open(self.path_metadata(), 'rb') as f:
+            self.dbx.files_upload(f.read(), self.dropbox_path_metadata(),
+                                  mode=dropbox.files.WriteMode("overwrite"))
+
+    def add_change(self, entry: Change) -> bool:
+        # TODO(mrostecki): Handle the version bump properly.
+        old_version = self.version
+        res = super().add_change(entry)
+        new_version = self.version
+        self.version = old_version
+        if self.pgp_key:
+            self.encrypt_file(self.path_current())
+        with open(self.path_current(), 'rb') as f:
+            self.dbx.files_upload(f.read(), self.dropbox_path_current(),
+                                  mode=dropbox.files.WriteMode("overwrite"))
+        self.version = new_version
+        return res
+
+
 def resolve_backend_class(backend_url):
     backend_map: Mapping[str, Type[Backend]] = {
+        'dropbox': DropboxBackend,
         'file': FileBackend,
+        'partial': PartialFileBackend,
     }
     p = urlparse(backend_url)
     backend_cl = backend_map.get(p.scheme, None)
     return backend_cl
 
 
-def get_backend(destination, create=False):
+def get_backend(destination: str, options: Mapping[str, str],
+                create=False, require_init=False):
     backend_cl = resolve_backend_class(destination)
     if backend_cl is None:
         raise ValueError("No backend implementation found for {destination}".format(
             destination=destination,
         ))
-    backend = backend_cl(destination, create=create)
-    backend.initialize()
+    backend = backend_cl(destination, options, create=create)
+    backend.validate_options(options)
+    initialized = backend.initialize()
+    if require_init and not initialized:
+        abort("Could not initialize the backup {}, please use 'backup-cli' to initialize the backup first.".format(destination))
     assert(backend.version is not None)
     assert(backend.prev_version is not None)
     return backend
@@ -330,9 +495,7 @@ def on_init(options: Mapping[str, str], plugin: Plugin, **kwargs):
     if not plugin.db_path.startswith('sqlite3'):
         abort("The backup plugin only works with the sqlite3 database.")
 
-    plugin.backend = get_backend(destination)
-    if not plugin.backend.initialize():
-        abort("Could not initialize the backup {}, please use 'backup-cli' to initialize the backup first.".format(destination))
+    plugin.backend = get_backend(destination, options, require_init=True)
 
     for c in plugin.early_writes:
         apply_write(plugin, c)
@@ -341,6 +504,14 @@ def on_init(options: Mapping[str, str], plugin: Plugin, **kwargs):
 plugin.add_option(
     'backup-destination', None,
     'Destination of the database backups (file:///filename/on/another/disk/).'
+)
+plugin.add_option(
+    'dropbox-token', os.environ.get('DROPBOX_TOKEN'),
+    'Access token for Dropbox'
+)
+plugin.add_option(
+    'pgp-key', None,
+    'PGP key (ID or email) to encrypt the database backup'
 )
 
 
