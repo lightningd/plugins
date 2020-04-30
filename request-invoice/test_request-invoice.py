9f412f900bfe95b48c1e1a6a9c14e34f28093a68
Christian Decker <decker.christian@gmail.com>
2020-04-30 21:31:07 +0200

    request-invoice: No need to kill background processes anymore

--- request-invoice/test_request-invoice.py
+++ request-invoice/test_request-invoice.py
@@ -35,13 +35,3 @@ def test_requestinvoice_starts(node_factory):
         r = requests.get('http://localhost:8809/invoice/1000/text')
     assert(r.status_code == 429)
     assert("429 Too Many Requests" in r.text )
-
-    l1.rpc.plugin_stop(plugin_path)
-    l1.stop()
-
-    # terminate background process
-    try:
-        full_path = "python3 " + plugin_path
-        check_output(["pkill","-f",  full_path])
-    except:
-        pass
