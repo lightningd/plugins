ff7360457b82ba50119027660986e647d0243c95
Michael Schmoock <michael@schmoock.net>
2020-05-28 12:52:33 +0200

    drain: utils - adds get_theirs helper

--- drain/utils.py
+++ drain/utils.py
@@ -26,19 +26,14 @@ def get_ours(node, scid):
     return [c for c in node.rpc.listfunds()['channels'] if c['short_channel_id'] == scid][0]['our_amount_msat']
 
 
-# these wait for the HTLC commit settlement
+# returns their_amount_msat for a given node and scid
+def get_theirs(node, scid):
+    ours = get_ours(node, scid)
+    total = [c for c in node.rpc.listfunds()['channels'] if c['short_channel_id'] == scid][0]['amount_msat']
+    return total - ours
+
+
+# these wait for the HTLC commit settlement to change our/their amounts
 def wait_ours(node, scid, ours_before):
     wait_for(lambda: ours_before != get_ours(node, scid))
     return get_ours(node, scid)
-
-
-def wait_ours_above(node, scid, value):
-    wait_for(lambda: get_ours(node, scid) > value)
-    return get_ours(node, scid)
-
-
-def wait_ours_below(node, scid, value):
-    wait_for(lambda: get_ours(node, scid) < value)
-    return get_ours(node, scid)
-
-
