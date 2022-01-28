a20796940491777e9af47f9daabd701ca200cf0d
svewa <svewa@users.noreply.github.com>
2022-01-28 15:53:07 +0100

    another instance of unchecked short_channel_id access in fee_strategy: get_fees_median (#330)
    * skip channels that are not (yet) locked in
    
    * another unchecked request to ch['short_channel_id']
    
    * this was meant to be the change.

--- feeadjuster/feeadjuster.py
+++ feeadjuster/feeadjuster.py
@@ -66,8 +66,9 @@ def get_ratio_hard(our_percentage):
 def get_peer_id_for_scid(plugin: Plugin, scid: str):
     for peer in plugin.peers:
         for ch in peer['channels']:
-            if ch['short_channel_id'] == scid:
-                return peer['id']
+            if 'short_channel_id' in ch:
+                if ch['short_channel_id'] == scid:
+                    return peer['id']
     return None
 
 
