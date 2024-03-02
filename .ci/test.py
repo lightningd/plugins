81f60f5de191b403aadffabf22d04fa127525a19
sip21 <sip21@proton.me>
2024-03-01 23:24:14 -0600

    Pin poetry to 1.7.1

--- .ci/test.py
+++ .ci/test.py
@@ -120,7 +120,7 @@ def prepare_env_poetry(p: Plugin, directory: Path) -> bool:
     subprocess.check_call(['which', 'python3'])
 
     subprocess.check_call([
-        pip3, 'install', '-U', *pip_opts, 'pip', 'wheel', 'poetry'
+        pip3, 'install', '-U', *pip_opts, 'pip', 'wheel', 'poetry==1.7.1'
     ], cwd=p.path.parent)
 
     # Install pytest (eventually we'd want plugin authors to include
