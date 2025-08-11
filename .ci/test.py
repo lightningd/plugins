f5fbcf5f8db1c1d5cdd64511f6fad23b47b03e16
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2025-08-11 12:41:15 +0200

    ci: upgrade nightly to uv

--- .ci/test.py
+++ .ci/test.py
@@ -193,12 +193,17 @@ def install_dev_pyln_testing(pip_path):
     cln_path = os.environ["CLN_PATH"]
 
     subprocess.check_call(
+        [pip_path, "install", *pip_opts, *global_dependencies],
+        stderr=subprocess.STDOUT,
+    )
+
+    subprocess.check_call(
         [
             pip_path,
             "install",
             *pip_opts,
-            "-r",
-            cln_path + "/requirements.txt",
+            cln_path + "/contrib/pyln-client",
+            cln_path + "/contrib/pyln-testing",
         ],
         stderr=subprocess.STDOUT,
     )
