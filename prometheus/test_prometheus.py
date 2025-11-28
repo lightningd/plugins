7d37dfb8c86dcdd51f7b81d5c436b7c5f73bab58
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2025-11-28 22:18:27 +0100

    prometheus: require python 3.9.2, add uv.lock, try fix port in use flake

--- prometheus/test_prometheus.py
+++ prometheus/test_prometheus.py
@@ -1,7 +1,6 @@
 import os
 from pyln.testing.fixtures import *  # noqa: F401,F403
 import urllib
-from ephemeral_port_reserve import reserve
 
 plugin_path = os.path.join(os.path.dirname(__file__), "prometheus.py")
 
@@ -21,13 +20,14 @@ def test_prometheus_starts(node_factory):
 def test_prometheus_scrape(node_factory):
     """Test that we can scrape correctly.
     """
-    l1 = node_factory.get_node(options={'plugin': plugin_path})
-    scrape = urllib.request.urlopen("http://localhost:9750")
+    port = node_factory.get_unused_port()
+    l1 = node_factory.get_node(options={'plugin': plugin_path, 'prometheus-listen': f'127.0.0.1:{port}'})
+    scrape = urllib.request.urlopen(f"http://localhost:{port}")
     
     
     
 def test_prometheus_channels(node_factory):
-    port = reserve()
+    port = node_factory.get_unused_port()
     l1, l2, l3 = node_factory.line_graph(
         3,
         opts=[
